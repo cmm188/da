bfe1914f5815d42cef61b0d4ac396c08f44d02cc
cmm188 <76857753+cmm188@users.noreply.github.com>
2021-04-26 00:23:14 +0800

    Update ne0cklace0.js

--- mumu/The.Flash/The.Flash/ne0cklace0.js
+++ mumu/The.Flash/The.Flash/ne0cklace0.js
@@ -8,7 +8,7 @@
 点点券，可以兑换无门槛红包（1元，5元，10元，100元，部分红包需抢购）
 活动入口：京东APP-领券中心/券后9.9-领点点券 [活动地址](https://h5.m.jd.com/babelDiy/Zeus/41Lkp7DumXYCFmPYtU3LTcnTTXTX/index.html)
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
-===============Quantumultx===============
+===============Quantumultx=============
 [task_local]
 #点点券
 10 0,20 * * * https://jdsharedresourcescdn.azureedge.net/jdresource/jd_necklace.js, tag=点点券, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
